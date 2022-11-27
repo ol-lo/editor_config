9b7a692b7bddfb9386ae80f6be18fdf5476fc816
Олег Ермаков <oleg.ermakov@fogstream.ru>
2022-11-28 02:19:55 +0300

    appended plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,8 +6,8 @@ return require("packer").startup(function()
     use 'lewis6991/impatient.nvim'
     use {'nvim-lua/plenary.nvim'}
     use {'kyazdani42/nvim-web-devicons'}
-	
-    -- themes 
+
+    -- themes
     use {
         "mcchrish/zenbones.nvim",
         -- Optionally install Lush. Allows for more configuration or extending the colorscheme
@@ -15,29 +15,32 @@ return require("packer").startup(function()
         -- In Vim, compat mode is turned on as Lush only works in Neovim.
         requires = "rktjmp/lush.nvim"
     }
+    use 'nvim-treesitter/nvim-treesitter'
     use("neovim/nvim-lspconfig")
     use("ray-x/lsp_signature.nvim")
     use 'simrat39/rust-tools.nvim'
+    use {'jose-elias-alvarez/typescript.nvim'}
 
     use({"glepnir/lspsaga.nvim", branch = "main"})
     use('rust-lang/rust.vim')
-	-- cmp
+    -- cmp
     use('hrsh7th/cmp-vsnip')
     use('hrsh7th/nvim-cmp')
     use('hrsh7th/cmp-nvim-lsp')
     use('hrsh7th/vim-vsnip')
     -- use('hrsh7th/cmp-buffer')
     use('hrsh7th/cmp-path')
-
     use {'stevearc/dressing.nvim'}
     use("williamboman/mason.nvim")
+    use {'williamboman/mason-lspconfig.nvim'}
 
     use({
         "jose-elias-alvarez/null-ls.nvim",
         requires = {{"nvim-lua/plenary.nvim"}}
     })
-
-    use('terrortylor/nvim-comment')
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    -- use('terrortylor/nvim-comment')
+    use('numToStr/Comment.nvim')
     use 'voldikss/vim-floaterm'
     use('machakann/vim-sandwich')
     use("windwp/nvim-autopairs")
